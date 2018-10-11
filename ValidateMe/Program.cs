4b71ee381d328c636e5fd1181072ffe594c01f24
Josh Zimmerman <JoshuaZimmerman@quickenloans.com>
2018-10-10 22:37:02 -0400

    This is my initial commit for DevBuild Lab 7, a program for data validation.

--- ValidateMe/Program.cs
+++ ValidateMe/Program.cs
@@ -10,6 +10,56 @@ namespace ValidateMe
     {
         static void Main(string[] args)
         {
+            bool validationResult = false;
+            List<ValidDataCheck> validDataChecks = new List<ValidDataCheck>();
+            string userPrompt = "";
+
+            #region Set up valid data headers and add them to List object
+            ValidDataCheck nameCheck = new ValidDataCheck(StringTable.NamePrompt, InformationType.Name, StringTable.ValidNameMessage, StringTable.InvalidNameMessage);
+            ValidDataCheck emailCheck = new ValidDataCheck(StringTable.EmailPrompt, InformationType.EmailAddress, StringTable.ValidEmailMessage, StringTable.InvalidEmailMessage);
+            ValidDataCheck phoneNumberCheck = new ValidDataCheck(StringTable.TelephoneNumberPrompt, InformationType.TelephoneNumber, StringTable.ValidTelephoneNumberMessage, StringTable.InvalidTelephoneNumberMessage);
+            ValidDataCheck dateCheck = new ValidDataCheck(StringTable.DatePrompt, InformationType.Date, StringTable.ValidDateMessage, StringTable.InvalidDateMessage);
+
+            validDataChecks.Add(nameCheck);
+            validDataChecks.Add(emailCheck);
+            validDataChecks.Add(phoneNumberCheck);
+            validDataChecks.Add(dateCheck);
+            #endregion
+
+            Console.Write("***********************************************************\n" +
+                          "*             Dev.Build(2.0) - Input Validation           *\n" +
+                          "***********************************************************\n\n");
+
+            while (true)
+            {
+                foreach (ValidDataCheck c in validDataChecks)
+                {
+                    PromptReentry:
+                    Console.Write("\n" + c.DataTypePrompt);   //Prompt the user for data to be validated
+                    validationResult = Validation.ValidateInfo(c.informationType, Console.ReadLine()); //have Validation class let us know if the data fits our format
+                    Console.WriteLine(validationResult == true ? c.validDataMessage : c.invalidDataMessage); //let the user know about Data Validation's findings
+                    if (!validationResult)
+                    {
+                        while (userPrompt != "y" && userPrompt != "n")      //if data is invalid, ask if the user would like to re-enter data
+                        {
+                            Console.Write(StringTable.ReEnterDataPrompt);
+                            userPrompt = Console.ReadLine().ToLower();
+                        }
+                        if (userPrompt == "y") { userPrompt = ""; goto PromptReentry; }
+                        else if (userPrompt == "n") { userPrompt = ""; continue; }
+                    }
+                }
+
+                while (userPrompt != "y" && userPrompt != "n")
+                {
+                    Console.Write(StringTable.RestartPrompt);
+                    userPrompt = Console.ReadLine().ToLower();
+                }
+                if (userPrompt == "y") { userPrompt = ""; continue; }
+                else if (userPrompt == "n") { userPrompt = ""; return; }
+            }
+
+
         }
     }
 }
---
+++ ValidateMe/Validation.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace ValidateMe
+{
+    public enum InformationType { Name = 0, EmailAddress, TelephoneNumber, Date, CustomField }
+
+
+    /// <summary>
+    /// This struct is used to hold an entry prompt string, an enum for the type of data being validated, 
+    /// and strings for display if entry is valid or invalid for that type
+    /// </summary>
+    public struct ValidDataCheck
+    {
+        public string DataTypePrompt;
+        public InformationType informationType;
+        public string validDataMessage;
+        public string invalidDataMessage;
+
+        public ValidDataCheck(string dataPrompt, InformationType type, string validMessage, string invalidMessage)
+        {
+            DataTypePrompt = dataPrompt;
+            informationType = type;
+            validDataMessage = validMessage;
+            invalidDataMessage = invalidMessage;
+        }
+    }
+
+    class Validation
+    {
+        private const string nameRegexPattern               = @"^[A-Z][a-z]{4,29}$";
+        private const string emailRegexPattern              = "^(([A-Za-z0-9]){1,30})+[@]+[a-zA-Z]{5,10}([.])+([A-Za-z.]{3,5})$";
+        private const string telephoneNumberRegexPattern    = @"^([(]?\d{3})[)-]\d{3}[-]\d{4}$";
+        private const string dateRegexPattern               = @"^(\d\d)+[/]+(\d{2})[/](\d){4}$";
+
+        private static readonly Dictionary<InformationType, string> InformationRegexes = 
+            new Dictionary<InformationType, string> {   { InformationType.Name, nameRegexPattern },
+                                                        { InformationType.EmailAddress, emailRegexPattern },
+                                                        { InformationType.TelephoneNumber, telephoneNumberRegexPattern },
+                                                        { InformationType.Date, dateRegexPattern },};
+
+        /// <summary>
+        /// This function validates information based on the type provided
+        /// </summary>
+        /// <param name="informationType">This is the information type to validate</param>
+        /// <param name="data">This is the string containing the information itself</param>
+        /// <returns></returns>
+        public static bool ValidateInfo(InformationType informationType, string data)
+        {
+            //use the information type argument to set up a regex with the pattern from our Dictionary
+            Regex r = new Regex(Validation.InformationRegexes[informationType]);
+            return r.IsMatch(data);
+        }
+
+
+        /// <summary>
+        /// Let's make this a bit like OpenGL and make it so the user can define their own custom regex pattern. 
+        /// Adding a custom field is not permanent as of this writing, and the custom pattern will be reset at runtime.
+        /// </summary>
+        /// <param name="customRegexPattern">User-generated regex pattern to be added to our dictionary</param>
+        public static void AddUserGeneratedRegex(string customRegexPattern)
+        {
+            try
+            {
+                if (!InformationRegexes.Keys.Contains(InformationType.CustomField))
+                {
+                    InformationRegexes.Add(InformationType.CustomField, customRegexPattern);
+                }
+                else throw new ArgumentException();
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine("A custom validation field has already been added. Multiple custom exceptions will be something we add down the line, but for now, you'll have to restart the program if you want to overwrite the custom field.");
+            }
+        }
+
+    }
+}
